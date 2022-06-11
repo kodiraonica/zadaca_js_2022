b3b3400328828ede7712f8736c46a20606a7759a
Christian Krajna <christian.krajna@gmail.com>
2022-06-11 09:40:56 +0200

    zadace.11.06

---
+++ varijable/zadatak1/main.js
@@ -0,0 +1,7 @@
+// console.log("test-connection");
+
+var x = 2;
+var y = 3;
+var xy = x * y;
+
+console.log(xy);
---
+++ varijable/zadatak2/main.js
@@ -0,0 +1,4 @@
+// console.log("testing connection");
+
+const gravity = 9.81;
+console.log(`Gravity = ${gravity} ms`);
